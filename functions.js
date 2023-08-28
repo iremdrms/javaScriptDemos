9429ca31829bf7cae28ec4becbda56ace3750349
iremdrms <durmus.irem@outlook.com>
2023-08-28 18:28:06 +0300

    .

--- functions.js
+++ functions.js
@@ -1,8 +1,22 @@
-var sayi1 = Math.ceil(Math.random() * 49)
-var sayi2 = Math.ceil(Math.random() * 49)
-var sayi3 = Math.ceil(Math.random() * 49)
-var sayi4 = Math.ceil(Math.random() * 49)
-var sayi5 = Math.ceil(Math.random() * 49)
-var sayi6 = Math.ceil(Math.random() * 49)
+var degisken = "Değer";
 
-console.log('Kolon : ' + sayi1 + ' ' + sayi2 + ' ' +sayi3 + ' ' +sayi4 + ' ' + sayi5 + ' ' + sayi6)
+function sayiUret(ustLimit = 49) {
+    console.log(degisken)
+    return Math.ceil(Math.random() * ustLimit)
+
+}
+
+console.log(degisken)
+
+//keyword
+
+sayiUret()
+
+var sayi1 = sayiUret(1000)
+var sayi2 = sayiUret()
+var sayi3 = sayiUret()
+var sayi4 = sayiUret()
+var sayi5 = sayiUret()
+var sayi6 = sayiUret()
+
+console.log('Kolon : ' + sayi1 + ' ' + sayi2 + ' ' + sayi3 + ' ' + sayi4 + ' ' + sayi5 + ' ' + sayi6)

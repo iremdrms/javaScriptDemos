491bf9dcf409f43fd86062b9d34d3d1d196bf4e8
iremdrms <durmus.irem@outlook.com>
2023-08-28 19:38:44 +0300

    .

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+var not = 21
+
+// not 50'den büyükse
+if (not>50){
+    console.log('Gectiniz')
+}else if (not>=40){
+    console.log('Butunlemeye kaldiniz')
+}
+else{
+    console.log('Kaldiniz')
+}
+
+// if(not<=50){
+//     console.log('Kaldınız')
+// }
---
+++ while.js
@@ -0,0 +1,15 @@
+ var sayi = 2
+
+ while(sayi<=100){ //infinite loop
+     console.log(sayi)
+    
+     sayi= sayi + 2
+ }
+
+// var sayi = 1
+
+// while(sayi<=100){
+//     if(sayi%2==0){
+//     console.log(sayi)}
+//     sayi=sayi+1
+// }
